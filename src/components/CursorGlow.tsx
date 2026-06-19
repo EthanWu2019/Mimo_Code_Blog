ca919e6c1db13c615e4c0b1454daea892864285a
EthanWu2019 <ethanwucz2019@gmail.com>
2026-06-18 21:36:57 -0500

    debug: add console.log to cursor morph functions

--- src/components/CursorGlow.tsx
+++ src/components/CursorGlow.tsx
@@ -121,6 +121,7 @@ export default function CursorGlow() {
       if (isTextRef.current) return;
       isTextRef.current = true;
       applyColor();
+      console.log('[CURSOR] morphToText: animating to', IBEAM_W, 'x', IBEAM_H);
       gsap.to(shape, {
         w: IBEAM_W, h: IBEAM_H,
         duration: 0.35,
@@ -129,6 +130,7 @@ export default function CursorGlow() {
         onUpdate() {
           wRef.current = shape.w;
           hRef.current = shape.h;
+          console.log('[CURSOR] onUpdate w=', shape.w.toFixed(1), 'h=', shape.h.toFixed(1));
           renderCursor();
         },
       });
@@ -138,6 +140,7 @@ export default function CursorGlow() {
       if (!isTextRef.current) return;
       isTextRef.current = false;
       applyColor();
+      console.log('[CURSOR] morphToDot: animating to', DOT_SIZE, 'x', DOT_SIZE);
       gsap.to(shape, {
         w: DOT_SIZE, h: DOT_SIZE,
         duration: 0.4,
